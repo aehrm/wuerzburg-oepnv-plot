1d04836ad2c530892e060eee9e746231ef983ab1
Anton Ehrmanntraut <anton@ehrmanntraut.de>
2025-07-23 16:48:44 +0200

    github link

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,14 @@ import { RouterOutlet } from "@angular/router";
   imports: [RouterOutlet],
   template: `
     <h1 class="main-title-container">
-      <span class="title-text">Würzburger ÖPNV Plot Generator</span>
+      <span class="title-text">
+        Würzburger ÖPNV Plot Generator
+        <a href="https://github.com/aehrm/wuerzburg-oepnv-plot">
+          <img
+            class="gh-icon"
+            src="https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png"
+          /> </a
+      ></span>
       <img
         src="https://www.wvv.de/media-wvv/mobilitaet/bilder/bus-und-strassenbahn/bus-und-straba_landscape_16_9_2.jpg"
       />
